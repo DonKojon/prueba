f53992708a4647a6ee10c9cf276b2bb392c54cf1
Jose Elias Saavedra <jelias626@gmail.com>
2025-05-27 21:07:33 -0400

    nuevo hola

---
+++ Frontend_/src/app/dashboard-director/dashboard-director.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-director',
+  imports: [],
+  templateUrl: './dashboard-director.component.html',
+  styleUrl: './dashboard-director.component.css'
+})
+export class DashboardDirectorComponent {
+
+}
---
+++ Frontend_/src/app/dashboard-estudiante/dashboard-estudiante.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-estudiante',
+  imports: [],
+  templateUrl: './dashboard-estudiante.component.html',
+  styleUrl: './dashboard-estudiante.component.css'
+})
+export class DashboardEstudianteComponent {
+
+}
